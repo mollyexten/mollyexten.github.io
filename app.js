bfc8f774bd8f2bf2afe16febf9808735cb018258
Molly Exten <molly.exten@gmail.com>
2022-12-26 06:39:31 -0500

    turned top nav header into link, removed button styling, removed js file again

--- app.js
+++
@@ -1,8 +0,0 @@
-const home = document.querySelector("#home button")
-
-home.addEventListener("click", scrollUp);
-
-function scrollUp() {
-    window.scrollTo(0, 0);    
-    window.location.href = window.location.href.split("#")[0];
-}
